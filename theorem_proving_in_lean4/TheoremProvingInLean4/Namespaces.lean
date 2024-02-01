27d6d83e78f72007942f5ad7068fb829c02060c4
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2024-02-01 17:26:00 -0300

    namespaces

---
+++ theorem_proving_in_lean4/TheoremProvingInLean4/Namespaces.lean
@@ -0,0 +1,24 @@
+namespace Foo
+  def a : Nat := 5
+  def f (x : Nat) : Nat := x + 7
+
+  def fa : Nat := f a
+  def ffa : Nat := f (f a)
+end Foo
+
+#check Foo.a -- Nat
+
+open Foo
+
+#check a -- Nat
+
+namespace Foo2
+  def a : Nat := 5
+  def f (x : Nat) : Nat := x + 7
+
+  def fa : Nat := f a
+
+  namespace Bar
+    def ffa : Nat := f (f a)
+  end Bar
+end Foo2
