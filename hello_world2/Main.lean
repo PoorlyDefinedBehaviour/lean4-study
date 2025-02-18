7ea4322fabf3310e1ea6f44126d25871faf778d9
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2025-02-18 22:39:56 +0100

    hello world

---
+++ hello_world2/Main.lean
@@ -0,0 +1,14 @@
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
+
+#eval 1 + 2
+
+#eval 1 + 2 * 5
+
+#eval String.append "Hello, " "world!"
+
+#eval String.append "great " (String.append "oak " "tree")
+
+#eval String.append "it is " (if 1 > 2 then "yes" else "no")
+
+#eval String.append "it is "
